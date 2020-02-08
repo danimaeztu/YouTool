865566f1531c6cce2dfa591238e45dfa3386169e
Dani Maeztu <daniel.maeztu@gmail.com>
2020-02-08 17:21:53 +0100

    upload files

--- SQL/CREATE USER.sql
+++ SQL/CREATE USER.sql
@@ -1,3 +1,3 @@
-CREATE USER 'youtool'@'localhost' IDENTIFIED BY '';
+CREATE USER 'youtool'@'localhost' IDENTIFIED BY 'PASSWORD';
 GRANT ALL PRIVILEGES ON YouTool . * TO 'youtool'@'localhost';
 FLUSH PRIVILEGES;
