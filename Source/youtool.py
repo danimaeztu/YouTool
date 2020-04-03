63b346bfc2933ba65cff59cfb002c843246d00be
Dani Maeztu <daniel.maeztu@gmail.com>
2020-04-04 00:22:00 +0200

    Print result

--- Source/youtool.py
+++ Source/youtool.py
@@ -175,3 +175,10 @@ connection = engine.connect()
 # Insert data into MariaDB Server
 GLOBAL.to_sql('Trending_videos', con=connection, schema='YouTool',
               if_exists='append', index=False)
+
+# Print the result
+sql = """SELECT country, COUNT(country) AS cantidad FROM Trending_videos
+         WHERE trending_date = "{}"
+         GROUP BY country;""".format(time.strftime("%y.%d.%m"))
+result = pd.read_sql(sql, connection)
+print(result)
